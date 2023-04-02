78de9401cde9f91dbb1d8980e8cca26b4861ee62
trukhinmakximo <trukhinmakximof@mail.ru>
2023-04-02 19:24:44 +0800

    (tested) module 'input_int' working properly, now will add functions

--- sources_1/new/input_int.v
+++ sources_1/new/input_int.v
@@ -17,23 +17,33 @@ module input_int(
     end
     
     always @ (posedge clk) begin
-        if (input_mode < 2'b11) begin
-            if (counter == max_counter) begin
+        if (counter == max_counter) begin
+            
+            if (input_mode == 2'b00) begin
                 case (buttons)
-                    5'b10000: begin 
-                        //number = number * 8'd10;
-                        number = number + 8'd1;
-                        $display("Number : %b\n", number);
-                    end
-                
+                    5'b10000: number = number * 8'd10;
+                    5'b01000: number = number * 8'd10 + 8'd1;
+                    5'b00100: number = number * 8'd10 + 8'd2;
+                    5'b00010: number = number * 8'd10 + 8'd3;
+                    5'b00001: number = number * 8'd10 + 8'd4;
+                endcase
+            
+            end
+            else if (input_mode == 2'b01) begin
+                case (buttons)
+                    5'b10000: number = number * 8'd10 + 8'd5;
+                    5'b01000: number = number * 8'd10 + 8'd6;
+                    5'b00100: number = number * 8'd10 + 8'd7;
+                    5'b00010: number = number * 8'd10 + 8'd8;
+                    5'b00001: number = number * 8'd10 + 8'd9;
                 endcase
             end
-        end
-        else begin
-            case (buttons)
-                5'b10000: number = 8'd0;
-                5'b01000: number = (number / 8'd10);
-            endcase
+            else if (input_mode == 2'b11) begin
+                case (buttons)
+                    5'b10000: number = 8'd0;
+                    5'b01000: number = (number / 8'd10);
+                endcase
+            end
         end
         if (counter == max_counter) counter = 0;
         else counter = counter + 1;
